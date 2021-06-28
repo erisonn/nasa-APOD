217d48a9fd50682a2d1bde020913472843b36fab
Erison <erisonn.santos@gmail.com>
2021-06-28 18:52:48 -0300

    css

--- js/index.js
+++ js/index.js
@@ -1,15 +1,21 @@
 const API_KEY = 'zV71fBPOCvdh97OH3Y3ELS1J7U1aoTkGabgbIEAl'
-var date_div = document.querySelector('.App')
+var app_div = document.querySelector('.App')
 var img_wrapper = document.querySelector('.img-wrapper')
 
-fetchAPI = async () => {
-    const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
-    const responseData = await response.json()
+fetchAPI = () => {
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
+    .then(response => response.json())
+    .then(responseText => showData(responseText))
+}
+
+showData = (responseText) => {
+    var responseData = responseText
+    console.log(responseData)
 
     //data da foto
     var createDate = document.createElement('p')
     createDate.innerHTML = responseData.date
-    date_div.appendChild(createDate)
+    app_div.appendChild(createDate)
 
     //foto
     var createIMG = document.createElement('img')
@@ -19,17 +25,16 @@ fetchAPI = async () => {
     //titulo
     var createTitle = document.createElement('p')
     createTitle.innerHTML = `<b>${responseData.title}</b>`
-    date_div.appendChild(createTitle)
+    app_div.appendChild(createTitle)
     //copyright
     var createCopyright = document.createElement('p')
     createCopyright.innerHTML = `<b>Image Credit & Copyright:</b> ${responseData.copyright}`
-    date_div.appendChild(createCopyright)
+    app_div.appendChild(createCopyright)
 
     //descrição
     var createDescription = document.createElement('p')
     createDescription.innerHTML = `<b>Explanation:</b> ${responseData.explanation}`
-    date_div.appendChild(createDescription)
+    app_div.appendChild(createDescription)
 }
-fetchAPI()
 
-
+fetchAPI();
